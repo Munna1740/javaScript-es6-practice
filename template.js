85723cd9e2dd31b84b458d67cb119cf5ca44247b
Munna1740 <mdmostafamunna@gmail.com>
2020-03-26 23:03:03 +0600

    template file added

---
+++ template.js
@@ -0,0 +1,19 @@
+const firstName = "MD Mostafa";
+const lastName = "Munna";
+const fullName = firstName+lastName + "is a good boy!!hahaha" ;
+const fullName2 = `${firstName} ${lastName} age:${20+2+2} is a good boy!!hahaha`;
+
+console.log(fullName);
+console.log(fullName2);
+
+//old multiline string:
+
+const multiline = "Hello!! How are you?\n"+
+                  "what are you doing?\n"+
+                  "Are you felling bore?";
+console.log(multiline);                  
+
+const multiline2 = `Hi!! ki obostha?
+koi acho ekhn?
+Dhaka naki gramer barite?`;
+console.log(multiline2);
---
+++ templete.js
@@ -0,0 +1,7 @@
+const firstName = "MD Mostafa";
+const lastName = "Munna";
+const fullName = firstName+lastName + "is a good boy!!hahaha" ;
+const fullName2 = `${firstName} ${lastName} age:${20+2+2} is a good boy!!hahaha`;
+
+console.log(fullName);
+console.log(fullName2);
