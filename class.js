594553ee4a748f716f5b3282e268840663462fe4
Munna1740 <mdmostafamunna@gmail.com>
2020-03-27 12:29:20 +0600

    class fil added

---
+++ class.js
@@ -0,0 +1,11 @@
+class Information{
+    constructor(id,name,address){
+        this.id = id;
+        this.name = name;
+
+
+    }
+}
+const student1 = new Information(163, "Munna");
+const student2 = new Information(182,"Mowri");
+console.log(student1,student2);
